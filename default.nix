50d55178b992438ba804d6d5b4553ece712cf52a
pyqlsa <26353308+pyqlsa@users.noreply.github.com>
2024-02-21 20:43:11 -0800

    fix for darwin builds

--- default.nix
+++ default.nix
@@ -1,4 +1,6 @@
 { lib
+, darwin
+, system
 , naersk
 , targetPlatform
 , pkg-config
@@ -12,6 +14,7 @@
 }:
 let
   cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+  optionalItems = cond: items: if cond then items else [ ];
 in
 naersk.lib."${targetPlatform.system}".buildPackage {
   src = ./.;
@@ -23,7 +26,7 @@ naersk.lib."${targetPlatform.system}".buildPackage {
     pkg-config
     libiconv
     openssl
-  ];
+  ] ++ (optionalItems (system == "aarch64-darwin") [ darwin.apple_sdk.frameworks.SystemConfiguration ]);
   checkInputs = [
     cargo
     rustc
